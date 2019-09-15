05acd141c84831f3a89e95558e110dfe21742f62
tnantoka <tnantoka@bornneet.com>
2019-09-15 23:05:51 +0900

    Add firebase to web

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+
 import './App.css';
+import firebase from './firebase';
+
+const db = firebase.firestore();
 
 const App: React.FC = () => {
-  return (
-    <div>
-      test
-    </div>
-  );
-}
+  useEffect(() => {
+    db.collection('test').add({ name: 'test' });
+  }, []);
+
+  return <div>test</div>;
+};
 
 export default App;
